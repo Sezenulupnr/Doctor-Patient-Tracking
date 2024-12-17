17fab5744f2a72f94ee3ba673f201bfed3c2d40c
Sezen Ulupınar <sezen.ulupnr@gmail.com>
2024-12-17 17:31:54 +0300

    Last Update

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 DOCTOR_DATABASE = [
-    {"name": "Sezen", "hospital": "Sezen", "specialty": "Sezen", "password": "Sezen"},
+    {"name": "Sezen Ulupınar", "hospital": "Medicana", "specialty": "Acil Tıp", "password": "12345"},
     {"name": "Seray Taşlı", "hospital": "Medipol", "specialty": "Dermatoloji", "password": "67890"},
     {"name": "Ali Veli", "hospital": "Acıbadem", "specialty": "Nöroloji", "password": "54321"},
 ]
